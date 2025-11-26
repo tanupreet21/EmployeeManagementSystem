558e7a078d56fe75d8e9d8808e1e5b4c647cde47
tanupreetkaur <tanubal21@gmail.com>
2025-11-26 15:32:23 -0500

    Created required components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Appbar, Toolbar, Button, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Navbar() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
+
+    return(
+        <Appbar position="static" sx={{ background: "#2e7d32"}}>
+            <Toolbar>
+
+                <Typography variant="h6" sx={{ flexGrow: 1}}>
+                    Employee Manager System
+                </Typography>
+
+                <Button color="inherit" component={Link} to="/employees">
+                    Employees
+                </Button>
+
+                <Button color="inherit" component={Link} to="/employees/add">
+                    Add Employee
+                </Button>
+
+                <Button color="inherit" onClick={handleLogout}>
+                    Logout
+                </Button>
+            </Toolbar>
+        </Appbar>
+    );
+}
