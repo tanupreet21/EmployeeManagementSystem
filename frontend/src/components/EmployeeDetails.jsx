fb31c48bb7c903533073a62bcb49351b5e37af0c
tanupreetkaur <tanubal21@gmail.com>
2025-11-27 19:01:36 -0500

    Added UI for EmployeeDetails & UpdateEmployee

--- frontend/src/components/EmployeeDetails.jsx
+++ frontend/src/components/EmployeeDetails.jsx
@@ -1,5 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Box, Card, Typography, Divider } from "@mui/material";
+import axios from "../api/axios";
+import { useParams } from "react-router-dom";
+
 export default function EmployeeDetails(){
+
+    const { id } = useParams();
+    const [emp, setEmp] = useState(null);
+
+    useEffect(() => {
+       axios.get('/employees/${id}').then((res)=> setEmp(res.data));
+    }, [id]);
+
+    if (!emp){
+        return <Typography p={4}>Loading...</Typography>;
+    }
     return (
-        <h1>Employee Details Page</h1>
+        <Box display="flex" justifyContent="center" minHeight="100vh" p={4}>
+            <Card sx={{width: 500, p:4}}>
+                <Typography variant="h5" mb={2}>
+                    Employee Details
+                </Typography>
+                <Divider/>
+
+                <Typography mt={2}><b>Name:</b> {emp.first_name} {emp.last_name}</Typography>
+                <Typography><b>Email:</b> {emp.email}</Typography>
+                <Typography><b>Position:</b> {emp.position}</Typography>
+                <Typography><b>Department:</b> {emp.department}</Typography>
+                <Typography><b>Salary:</b> ${emp.salary}</Typography>
+                <Typography><b>Date of Joining:</b> {new Date(emp.date_of_joining).toLocaleDateString()}</Typography>
+
+                <Divider sx={{ my: 2 }} />
+
+                <Typography variant="caption" color="gray">
+                Created: {new Date(emp.created_at).toLocaleString()}
+                <br />
+                Updated: {new Date(emp.updated_at).toLocaleString()}
+                </Typography>
+            </Card>
+        </Box>
     );
 }
--- frontend/src/components/UpdateEmployee.jsx
+++ frontend/src/components/UpdateEmployee.jsx
@@ -1,5 +1,205 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  TextField,
+  Button,
+  Typography,
+  MenuItem,
+} from "@mui/material";
+import axios from "../api/axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const departments = ["HR", "Engineering", "Finance", "Marketing"];
+const positions = ["Manager", "Developer", "Analyst", "Designer"];
+
 export default function UpdateEmployee(){
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [ form, setForm ] = useState({
+        first_name:"",
+        last_name:"",
+        email:"",
+        position:"",
+        salary:"",
+        date_of_joining:"",
+        department:""
+    });
+
+    const [errors, setErrors] = useState({});
+    const[loading, setLoading] = useState(true);
+
+    // Fetch employee details to prefill the form
+    useEffect(() => {
+        const fetchEmployee = async () => {
+            try {
+                const res = await axios.get('/employees/${id}');
+
+                setForm({
+                    first_name: res.data.first_name,
+                    last_name: res.data.last_name,
+                    email: res.data.email,
+                    position: res.data.position,
+                    salary: res.data.salary,
+                    date_of_joining: res.data.date_of_joining?.split("T")[0],
+                    department: res.data.department
+                });
+                setLoading(false);
+            } catch (error){
+                console.error("Error fetching employee:", error);
+            }
+        }
+        fetchEmployee();
+    }, [id]);
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const validate = () => {
+        const err = {};
+        if (!form.first_name.trim()) err.first_name = "First name is required";
+        if (!form.last_name.trim()) err.last_name = "Last name is required";
+        if (!form.email.trim()) err.email = "Email is required";
+        if (!form.position.trim()) err.position = "Position is required";
+        if (!form.salary) err.salary = "Salary is required";
+        if (!form.date_of_joining) err.date_of_joining = "Join date required";
+        if (!form.department.trim()) err.department = "Department required";
+    
+        setErrors(err);
+        return Object.keys(err).length === 0;
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (!validate()) return;
+    
+        try {
+          await axios.put(`/employees/${id}`, form);
+          navigate("/employees");
+        } catch (error) {
+          console.error("Error updating employee:", error);
+        }
+      };
+    
+      if (loading) {
+        return (
+          <Typography p={4} textAlign="center">
+            Loading...
+          </Typography>
+        );
+      }
+
     return (
-        <h1>Update Employee Page</h1>
+        <Box>
+            <Card sx={{ width: 500, p: 4 }}>
+                <Typography variant="h5" mb={2}>
+                    Edit Employee
+                </Typography>
+
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        fullWidth
+                        label="First Name"
+                        name="first_name"
+                        margin="normal"
+                        value={form.first_name}
+                        onChange={handleChange}
+                        error={Boolean(errors.first_name)}
+                        helperText={errors.first_name}
+                    />
+
+                    <TextField
+                        fullWidth
+                        label="Last Name"
+                        name="last_name"
+                        margin="normal"
+                        value={form.last_name}
+                        onChange={handleChange}
+                        error={Boolean(errors.last_name)}
+                        helperText={errors.last_name}
+                    />
+
+                    <TextField
+                        fullWidth
+                        label="Email"
+                        name="email"
+                        margin="normal"
+                        value={form.email}
+                        onChange={handleChange}
+                        error={Boolean(errors.email)}
+                        helperText={errors.email}
+                    />
+
+                    <TextField
+                        select
+                        fullWidth
+                        label="Position"
+                        name="position"
+                        margin="normal"
+                        value={form.position}
+                        onChange={handleChange}
+                        error={Boolean(errors.position)}
+                        helperText={errors.position}
+                    >
+                        {positions.map((p) => (
+                        <MenuItem key={p} value={p}>
+                            {p}
+                        </MenuItem>
+                        ))}
+                    </TextField>
+
+                    <TextField
+                        fullWidth
+                        label="Salary"
+                        name="salary"
+                        type="number"
+                        margin="normal"
+                        value={form.salary}
+                        onChange={handleChange}
+                        error={Boolean(errors.salary)}
+                        helperText={errors.salary}
+                    />
+
+                    <TextField
+                        fullWidth
+                        label="Date of Joining"
+                        name="date_of_joining"
+                        type="date"
+                        margin="normal"
+                        InputLabelProps={{ shrink: true }}
+                        value={form.date_of_joining}
+                        onChange={handleChange}
+                        error={Boolean(errors.date_of_joining)}
+                        helperText={errors.date_of_joining}
+                    />
+
+                    <TextField
+                        select
+                        fullWidth
+                        label="Department"
+                        name="department"
+                        margin="normal"
+                        value={form.department}
+                        onChange={handleChange}
+                        error={Boolean(errors.department)}
+                        helperText={errors.department}
+                    >
+                        {departments.map((d) => (
+                        <MenuItem key={d} value={d}>
+                            {d}
+                        </MenuItem>
+                        ))}
+                    </TextField>
+
+                    <Button variant="contained" fullWidth sx={{ mt: 2 }} type="submit">
+                        Update
+                    </Button>
+                </form>
+            </Card>  
+        </Box>
     );
-}
+};
