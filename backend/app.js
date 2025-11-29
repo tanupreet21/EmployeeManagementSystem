0cd0566768784eb4e030db4fc12b1d8c9155c073
tanupreetkaur <tanubal21@gmail.com>
2025-11-28 19:06:52 -0500

    Added app.js back - it was deleted accidently

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const initDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const app = express();
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "http://localhost:3001", // host
+  "http://frontend:3000" // container-to-container
+];
+
+app.use(express.json()); // parse requests that have JSON body
+app.use('/uploads', express.static('uploads'));
+
+
+// Routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/emp', employeeRoutes);
+
+//Global error handler
+app.use(errorHandler);
+
+//Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
+
+initDB(); // Connect to DB
